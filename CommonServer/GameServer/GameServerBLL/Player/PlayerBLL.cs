28f3d811302ab957b72d7bb838762504d7077b1c
肖强 <xiaoqiang@moqihuyu.com>
2017-05-04 10:27:15 +0800

    哈哈

---
+++ CommonServer/GameServer/GameServerBLL/Player/PlayerBLL.cs
@@ -0,0 +1,22 @@
+﻿/************************************************************************
+* 标题: 玩家类
+* 描述: 玩家类
+* 作者： 肖强
+* 日期：2017-5-3 10:43:36
+* 版本：V1
+*************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.BLL.Player
+{
+    /// <summary>
+    /// 玩家类
+    /// </summary>
+    public static class PlayerBLL
+    {
+    }
+}
