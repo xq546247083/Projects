7facb865d9144e01f28c578fe37cdb82bf46c0fc
肖强 <xiaoqiang@moqihuyu.com>
2017-07-13 19:39:50 +0800

    第三次提交

---
+++ CommonServer/GameServer/GameServerBLL/Test/I_Test.cs
@@ -0,0 +1,67 @@
+﻿/************************************************************************
+* 标题: 玩家类
+* 描述: 玩家类
+* 作者： 肖强
+* 日期：2017-5-3 10:43:36
+* 版本：V1
+*************************************************************************/
+using System;
+
+
+namespace GameServer.BLL
+{
+    using Tool.CustomAttribute;
+
+    /// <summary>
+    /// 玩家类
+    /// </summary>
+    [InvokeClassAttribute("玩家类", "肖强", "2017-7-13 10:44:02")]
+    public static partial class TestLBLL
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="serverId">serverId</param>
+        /// <param name="userId">userId</param>
+        /// <param name="userPwd">userPwd</param>
+        /// <param name="inputEncryptedString">inputEncryptedString</param>
+        /// <param name="random">random</param>
+        [MethodDescribe(
+            "Test", "肖强", "2017-7-13 10:59:13",
+            @"
+[
+]
+            ",
+            @"
+[
+]
+            ")]
+        public static void I_Test(String serverId, String userId, String userPwd, String inputEncryptedString, String random)
+        {
+
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="serverId">serverId</param>
+        /// <param name="userId">userId</param>
+        /// <param name="userPwd">userPwd</param>
+        /// <param name="inputEncryptedString">inputEncryptedString</param>
+        /// <param name="random">random</param>
+        [MethodDescribe(
+            "Test2", "肖强", "2017-7-13 10:59:13",
+            @"
+[
+]
+            ",
+            @"
+[
+]
+            ")]
+        public static void I_Test2(String serverId, String userId, String userPwd, String inputEncryptedString, String random)
+        {
+
+        }
+    }
+}
