222e72b0bbe7a4c7f088ad60386d482069c11b82
肖强 <xiaoqiang@moqihuyu.com>
2017-07-17 11:24:18 +0800

    提交代码

---
+++ CommonServer/GameServer/GameServerBLL/Interface/ICheck.cs
@@ -0,0 +1,25 @@
+﻿/************************************************************************
+* 标题: 配置接口
+* 描述: 接口
+* 作者： 肖强
+* 日期：2017-5-3 10:43:36
+* 版本：V1
+*************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace GameServer.BLL
+{
+    /// <summary>
+    /// 检测数据
+    ///</summary>
+    public interface ICheck
+    {
+        /// <summary>
+        /// 检测数据
+        /// <returns>错误信息</returns>
+        /// </summary>
+        List<String> Check();
+    }
+}
---
+++ CommonServer/GameServer/GameServerModel/CommonModel/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace GameServer.Model.CommonModel
+{
+    public interface IModel
+    {
+        /// <summary>
+        /// 构造对象
+        /// </summary>
+        /// <param name="dr">要构造的数据行</param>
+        void Construct(DataRow dr);
+    }
+}
