62155011dcb186435f2234f67b035b5105c6312e
肖强 <xiaoqiang@moqihuyu.com>
2017-07-26 15:58:38 +0800

    蜘蛛提交

---
+++ Spider/Spider/Model/Link.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Spider.Model
+{
+    /// <summary>
+    /// 链接model
+    /// </summary>
+    public class Link
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Uri
+        /// </summary>
+        public Uri Uri { get; set; }
+
+        /// <summary>
+        /// Count
+        /// </summary>
+        public Int32 Count { get; set; }
+    }
+}
---
+++ Spider/Spider/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+
+namespace Spider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BLL.Spider spider = new BLL.Spider();
+            spider.Start(ConfigurationManager.AppSettings["url"]);
+            Console.WriteLine("已经爬完了！！！");
+            Console.ReadKey();
+        }
+    }
+}
