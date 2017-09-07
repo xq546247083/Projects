f05afeac03913b5ea3c89041e7dac528067aa1e6
肖强 <xiaoqiang@moqihuyu.com>
2017-09-07 12:27:49 +0800

    删除本地文件

---
+++ WebServer/WebServer/WebServerModel/Role/SysRole.cs
@@ -0,0 +1,43 @@
+﻿/************************************************************************
+* 标题: sys_role
+* 描述: sys_role
+* 数据表:sys_role
+* 作者：徐敏荣
+* 日期：2017/9/7 12:23:43
+* 版本：V1.0
+*************************************************************************/
+
+using System;
+using System.Data;
+
+namespace WebServer.Model
+{
+    using Tool.CustomAttribute;
+
+    /// <summary>
+    /// sys_role
+    /// </summary>
+    [DataBaseTable("sys_role")]
+    public class SysRole
+    {
+        #region 属性
+
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [PrimaryKeyAttribute]
+        public Guid RoleID { set; get; }
+
+        /// <summary>
+        /// 角色名称
+        /// </summary>
+        public String RoleName { set; get; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public String Notes { set; get; }
+
+        #endregion
+    }
+}
