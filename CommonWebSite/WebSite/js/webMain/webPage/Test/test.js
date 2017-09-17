88191e3a5f1e56c3be3b063c07f040db29fbe27a
xiaoqiang <546247083@qq.com>
2017-09-17 13:11:09 +0800

    图片上传功能完成

--- CommonWebSite/WebSite/js/webMain/webPage/Test/test.js
+++ CommonWebSite/WebSite/js/webMain/webPage/Test/test.js
@@ -43,13 +43,35 @@ jQuery(
                 extensions: "gif,jpg,jpeg,bmp,png", 
                 mimeTypes: "image/jpg,image/jpeg,image/png"
             },
-            compress:false,
+            compress:{
+                width: 1600,
+                height: 1600,
+            
+                // 图片质量，只有type为`image/jpeg`的时候才有效。
+                quality: 90,
+            
+                // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
+                allowMagnify: false,
+            
+                // 是否允许裁剪。
+                crop: false,
+            
+                // 是否保留头部meta信息。
+                preserveHeaders: true,
+            
+                // 如果发现压缩后文件大小比原来还大，则使用原来图片
+                // 此属性可能会影响图片自动纠正功能
+                noCompressIfLarger: false,
+            
+                // 单位字节，如果图片大小小于此值，不会采用压缩。
+                compressSize: 0
+            },
             disableGlobalDnd: !0,
             fileNumLimit: 10,
             fileSizeLimit: 5242880,
             fileSingleSizeLimit: 5242880,
             chunked: true,//开启分片上传  
-            chunkSize:1024*100,// 如果要分片，分多大一片？默认大小为5M  
+            nkSize:1024*100,// 如果要分片，分多大一片？默认大小为5M  
             chunkRetry: 3,//如果某个分片由于网络问题出错，允许自动重传多少次  
             threads: 1,//上传并发数。允许同时最大上传进程数[默认值：3]  
             duplicate : false,//是否重复上传（同时选择多个一样的文件），true可以重复上传  
@@ -111,28 +133,3 @@ jQuery(
             }), c.addClass("state-" + k), s()
     }
 );
-
-//--------------------------------压缩代码-----------------
-// compress:{
-//     width: 1600,
-//     height: 1600,
-
-//     // 图片质量，只有type为`image/jpeg`的时候才有效。
-//     quality: 90,
-
-//     // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
-//     allowMagnify: false,
-
-//     // 是否允许裁剪。
-//     crop: false,
-
-//     // 是否保留头部meta信息。
-//     preserveHeaders: true,
-
-//     // 如果发现压缩后文件大小比原来还大，则使用原来图片
-//     // 此属性可能会影响图片自动纠正功能
-//     noCompressIfLarger: false,
-
-//     // 单位字节，如果图片大小小于此值，不会采用压缩。
-//     compressSize: 0
-// },
