13953f5a4c89fe72016e9dd0e352358ccb266fde
xiaoqiang <546247083@qq.com>
2017-09-23 02:06:52 +0800

    完成代码生成器

--- CommonWebSite/WebServer/WebServerModel/Blog/UBlog.cs
+++ CommonWebSite/WebServer/WebServerModel/Blog/UBlog.cs
@@ -1,9 +1,9 @@
 ﻿/************************************************************************
-* 标题: u_blog
-* 描述: u_blog
+* 标题: 博客表
+* 描述: 博客表
 * 数据表:u_blog
 * 作者：xiaoqiang
-* 日期：2017/9/10 0:25:41
+* 日期：2017/9/23 2:06:23
 * 版本：V1.0
 *************************************************************************/
 
@@ -15,7 +15,7 @@ namespace WebServer.Model
     using Tool.CustomAttribute;
 
     /// <summary>
-    /// u_blog
+    /// 博客表
     /// </summary>
     [DataBaseTable("u_blog")]
     public class UBlog
@@ -25,53 +25,53 @@ namespace WebServer.Model
         /// <summary>
         /// 主键
         /// </summary>
-        [PrimaryKeyAttribute]
-        public Guid ID { set; get; }
+        [PrimaryKey]
+        public Guid ID { get; set; }
 
         /// <summary>
         /// 用户id
         /// </summary>
-        public Guid UserId { set; get; }
+        public Guid UserId { get; set; }
 
         /// <summary>
         /// 标题
         /// </summary>
-        public String Title { set; get; }
+        public String Title { get; set; }
 
         /// <summary>
         /// 内容
         /// </summary>
-        public String Content { set; get; }
+        public String Content { get; set; }
 
         /// <summary>
         /// 标签（用，号隔开）
         /// </summary>
-        public String Tag { set; get; }
+        public String Tag { get; set; }
 
         /// <summary>
         /// @的用户
         /// </summary>
-        public String ATUsers { set; get; }
+        public String ATUsers { get; set; }
 
         /// <summary>
         /// 博客类型
         /// </summary>
-        public Int32 BlogType { set; get; }
+        public Int32 BlogType { get; set; }
 
         /// <summary>
         /// 状态【0：草稿，1：正常，2：删除，3：彻底删除】
         /// </summary>
-        public Int32 Status { set; get; }
+        public Byte Status { get; set; }
 
         /// <summary>
         /// 创建时间
         /// </summary>
-        public DateTime CrDate { set; get; }
+        public DateTime CrDate { get; set; }
 
         /// <summary>
         /// 更新时间
         /// </summary>
-        public DateTime ReDate { set; get; }
+        public DateTime ReDate { get; set; }
 
         #endregion
     }
