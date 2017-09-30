600feaf091db1c33bcfe971cc531c596d2336265
肖强 <xiaoqiang@moyihuyu.com>
2017-09-30 10:02:22 +0800

    被封数据库

--- CommonCore/FileBase/SqlCode/webserver.sql
+++ CommonCore/FileBase/SqlCode/webserver.sql
@@ -12,6 +12,10 @@ MySQL - 5.7.12-log : Database - webserver
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`webserver` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `webserver`;
+
 /*Table structure for table `sys_menu` */
 
 DROP TABLE IF EXISTS `sys_menu`;
