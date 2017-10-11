82661852b9ff66659068a79db32568fad2d5245a
肖强 <xiaoqiang@moyihuyu.com>
2017-10-11 10:10:10 +0800

    添加tolist

--- CommonWebSite/WebServer/WebServerBLL/Common/ExtensionBLL.cs
+++ CommonWebSite/WebServer/WebServerBLL/Common/ExtensionBLL.cs
@@ -22,21 +22,34 @@ namespace WebServer.BLL
         #region DataTable
 
         /// <summary>
-        /// 扩展方法类
+        /// DataTable转化泛型实体
         /// </summary>
-        /// <typeparam name="T">类型</typeparam>
-        /// <param name="dt">数据</param>
-        /// <returns>返回List</returns>
-        public static List<T> ToList<T>(this DataTable dt) where T : class,IModel, new()
+        /// <typeparam name="TResult">返回类型</typeparam>
+        /// <param name="dt"></param>
+        /// <returns>TResult的泛型</returns>
+        public static List<TResult> ToList<TResult>(this DataTable dt) where TResult : class, new()
         {
-            List<T> resultList = new List<T>();
-            foreach (DataRow dr in dt.Rows)
-            {
-                //构造对象
-                T tempObject = new T();
-                tempObject.Construct(dr);
+            //创建一个属性的列表  
+            List<PropertyInfo> propertyInfoList = new List<PropertyInfo>();
 
-                resultList.Add(tempObject);
+            //获取TResult的类型实例  反射的入口  
+            Type t = typeof(TResult);
+
+            //获得TResult 的所有的Public 属性 并找出TResult属性和DataTable的列名称相同的属性(PropertyInfo) 并加入到属性列表   
+            Array.ForEach<PropertyInfo>(t.GetProperties(), p => { if (dt.Columns.IndexOf(p.Name) != -1) propertyInfoList.Add(p); });
+
+            //创建返回的集合  
+            List<TResult> resultList = new List<TResult>();
+            foreach (DataRow row in dt.Rows)
+            {
+                //创建TResult的实例  
+                TResult result = new TResult();
+
+                //找到对应的数据  并赋值  
+                propertyInfoList.ForEach(p => { if (row[p.Name] != DBNull.Value) p.SetValue(result, Convert.ChangeType(row[p.Name], p.PropertyType), null); });
+
+                //放入到返回的集合中.  
+                resultList.Add(result);
             }
 
             return resultList;
