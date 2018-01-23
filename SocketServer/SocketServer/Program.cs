10673384829572dccba324bf9494bd57769a053d
肖强 <xiaoqiang@moqihuyu.com>
2018-01-23 21:50:03 +0800

    优化了代码

--- SocketServer/SocketServer/Program.cs
+++ SocketServer/SocketServer/Program.cs
@@ -3,11 +3,10 @@
 *************************************************************************/
 using System;
 using System.IO;
-using Tool.Common;
 
 namespace SocketServer
 {
-
+    using Tool.Common;
 
     /// <summary>
     /// 启动
@@ -21,6 +20,7 @@ namespace SocketServer
         static void Main(string[] args)
         {
             Init();
+            StartServer();
 
             Console.WriteLine("服务器启动成功！");
             Log.Info("服务器启动成功！");
@@ -43,13 +43,28 @@ namespace SocketServer
 
                 // 设置邮件信息
                 EmailTool.SetSenderInfo(SocketServerConfig.EmailHost, SocketServerConfig.EmailAddress, SocketServerConfig.EmailPass);
+            }
+            catch (Exception ex)
+            {
+                var exStr = $"服务器初始化失败。错误信息：{ex}";
+                Console.WriteLine(exStr);
+                Log.Error(exStr);
+            }
+        }
 
+        /// <summary>
+        /// 开启服务器
+        /// </summary>
+        static void StartServer()
+        {
+            try
+            {
                 // 启动回调服务器
                 CallbackServerManager.Start(SocketServerConfig.CallbackServerUrl);
             }
             catch (Exception ex)
             {
-                var exStr = $"服务器初始化失败。错误信息：{ex}";
+                var exStr = $"服务器启动失败。错误信息：{ex}";
                 Console.WriteLine(exStr);
                 Log.Error(exStr);
             }
