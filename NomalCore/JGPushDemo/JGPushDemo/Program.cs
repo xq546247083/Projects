f79e2a4e42f8ff8761b0897ea8ea95bc9ff9824f
肖强 <xiaoqiang@moqihuyu.com>
2018-04-27 11:13:01 +0800

    极光推送修正

--- NomalCore/JGPushDemo/JGPushDemo/Program.cs
+++ NomalCore/JGPushDemo/JGPushDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JGPushDemo
 {
@@ -8,12 +9,30 @@ namespace JGPushDemo
         {
             while (true)
             {
-                Console.Write("请输入要推送的标题:");
-                var title = Console.ReadLine();
-                Console.Write("请输入要推送的内容:");
-                var content = Console.ReadLine();
+                Console.Write("推送类型：是否全部推送（Y/N）:");
+                var flag = Console.ReadLine();
+                var dasd = flag.ToLower()[0];
+                if (flag.Length >= 1 && flag.ToLower()[0].Equals('y'))
+                {
+                    Console.Write("请输入要推送的标题:");
+                    var title = Console.ReadLine();
+                    Console.Write("请输入要推送的内容:");
+                    var content = Console.ReadLine();
 
-                JGPush.SendAll(title, content);
+                    JGPush.SendAll(title, content);
+                }
+                else
+                {
+                    Console.Write("请输入要推送的注册ID:");
+                    var regID = Console.ReadLine();
+                    Console.Write("请输入要推送的标题:");
+                    var title = Console.ReadLine();
+                    Console.Write("请输入要推送的内容:");
+                    var content = Console.ReadLine();
+
+                    JGPush.Send(new List<String>() { regID }, title, content);
+                }
+
                 Console.WriteLine("推送成功！");
                 Console.WriteLine();
             }
